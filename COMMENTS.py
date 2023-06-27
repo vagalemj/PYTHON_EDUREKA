aec725538c370c9bec87872898a234f7ee8bd452
vagalemj <mj.vagale@gmail.com>
2023-06-27 19:10:20 +0530

    PYTHON

---
+++ COMMENTS.py
@@ -0,0 +1,17 @@
+# THIS IS A COMMENTS
+print("comment test")
+
+#ADDITION
+#PRINT THE ADDITION
+a=2
+b=3
+print(a+b)
+
+#DOCSTRINGS
+a=2
+b=3
+c=a+b
+print(c)
+"""
+docstring comments
+"""
---
+++ TEST.py
@@ -0,0 +1,6 @@
+m=10
+print(m)
+if (m==1):
+    print("hey")
+else:
+    print("hello")
