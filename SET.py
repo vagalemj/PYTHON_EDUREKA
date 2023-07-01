0fee102cccf910255b244b1d3c6c117ba7ba4464
vagalemj <mj.vagale@gmail.com>
2023-07-01 20:31:14 +0530

    PYTHON

---
+++ SET.py
@@ -0,0 +1,5 @@
+myset={10,20,30,30,'madhu','sudhan','sudhan'}   #declaring set
+
+print(myset)    #no duplicate entries
+
+#can't access duplicate values
---
+++ TUPLE.py
@@ -0,0 +1,7 @@
+animals=(10,20,30,40,'tiger','lion','bear')     #declaring tuple
+
+print(animals) 
+
+print(animals[4]) #accessing the 4th value in tuple
+
+print(animals.count('tiger'))   #count of the word tiger in tuple
