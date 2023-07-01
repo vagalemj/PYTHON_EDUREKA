385afb4669b5aca6cfc567bd5c4c4bbe0abf8109
vagalemj <mj.vagale@gmail.com>
2023-07-01 20:52:11 +0530

    PYTHON

---
+++ DATA_TYPES_MISC(EXAMPLE).py
@@ -0,0 +1,13 @@
+a=[1,2,3,4,5]                      #LIST
+
+b=('madhu',10,20)                  #TUPLE
+
+c={1:'hey',2:'hello',3:'hiee'}     #DICTIONARY
+
+d=[a,b,c]                          #COMBINING ALL TO LIST
+
+e=(a,b,c)                          #COMBINING ALL TO TUPLE RECORD
+
+print(d)                           #PRINTING LIST
+
+print(e)                           #PRINTING TUPLE
---
+++ RANGE.py
@@ -0,0 +1,3 @@
+print(range(10)) #knowing the range of specific number
+
+print(list(range(26)))  #printing the list of all specified numbers
