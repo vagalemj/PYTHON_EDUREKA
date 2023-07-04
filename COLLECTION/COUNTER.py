0b8ecd922494d40eb127446c3d6941c5e39e27fb
vagalemj <mj.vagale@gmail.com>
2023-07-04 18:53:02 +0530

    PYTHON

--- COLLECTION/COUNTER.py
+++ COLLECTION/COUNTER.py
@@ -1,1 +1,16 @@
+from collections import Counter
 
+a=[1,2,2,1,3,4,5,5,5,3,1,2,3,8,8,1,7]
+b=Counter(a)        #counts the number of occurance of values inside list
+
+print(b)
+
+print(list(b.elements())) #returns list containing all the elements (ordered)
+
+print(b.most_common()) #returns sorted list with count
+
+sub={2:1, 1:1}
+
+print(b.subtract(sub))  #subtraction of number of elements
+
+print(b.most_common())
