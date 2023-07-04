66a30b495e6f716a30e7d887ecbd8e31fdf33551
vagalemj <mj.vagale@gmail.com>
2023-07-04 20:12:15 +0530

    PYTHON

---
+++ ARRAYS/ACCESSING_ELEMENTS.py
@@ -0,0 +1,8 @@
+import array as arr
+
+a = arr.array('i', [1, 2, 3, 4, 5, 6])
+print(a)
+a = arr.array('f', [1, 2, 3, 4, 5, 6])
+print(a)
+
+print(a[2])
---
+++ ARRAYS/ARRAY_MODULE_ARR.py
@@ -0,0 +1,5 @@
+import array as arr
+
+a = arr.array('i', [1, 2, 3, 4, 5, 6])
+
+print(a)
