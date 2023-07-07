6b53c768f74c7337beb41409bb55fa6319446eec
vagalemj <mj.vagale@gmail.com>
2023-07-08 02:35:24 +0530

    PYTHON

--- ARRAYS/ARRAY_CONCATENATION.py
+++ ARRAYS/ARRAY_CONCATENATION.py
@@ -1,7 +1,7 @@
 import array as arr
 
-a = arr.m('f', [1, 2, 3, 4, 5])
-b = arr.m('f', [6, 7, 8, 9, 10])
+a = arr.array('f', [1, 2, 3, 4, 5])
+b = arr.array('f', [6, 7, 8, 9, 10])
 c = a + b       #concatenation
 
-print('array concat: ', c)
+print('array concat: ', c) 
---
+++ ARRAYS/ARRAY_SLICING.py
@@ -0,0 +1,13 @@
+import array as arr
+
+a=arr.array('f',[1,2,3,4,5,6,7,8,9,10])
+
+print(a[0:3])   #index range from 0 to 3
+
+print(a[1:])    #index range from 1 all
+
+print(a[:4])    #index range from 0 to 4
+
+print(a[3:9])   #index range from 3 to 9
+
+print(a[::-1])  #reverse all
