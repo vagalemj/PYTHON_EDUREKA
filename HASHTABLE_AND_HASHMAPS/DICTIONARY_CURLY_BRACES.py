c652b021a9c64608c5be9454a0337dc3b64d91fb
vagalemj <mj.vagale@gmail.com>
2023-07-10 20:07:35 +0530

    PYTHON

--- HASHTABLE_AND_HASHMAPS/DICTIONARY_CURLY_BRACES.py
+++ HASHTABLE_AND_HASHMAPS/DICTIONARY_CURLY_BRACES.py
@@ -1,9 +1,12 @@
-my_dict = {'madhu': '001', 'sudhan': '002', 'vagale': '003'}
+my_dict = {'madhu': '001', 'sudhan': '002', 'vagale': '003'}    #new dictionary using curly braces
 print(my_dict)
 print(type(my_dict))
 
-new_dict = dict(j=500, v=600)
+new_dict = dict(j=500, v=600)   #new dictionary using function with parameters
 print(new_dict)
 print(type(new_dict))
 
-print(my_dict, new_dict)
+mj_dict = dict()        #empty dictionary
+print(mj_dict)
+
+print(my_dict, mj_dict, new_dict)   #printing dictionaries
