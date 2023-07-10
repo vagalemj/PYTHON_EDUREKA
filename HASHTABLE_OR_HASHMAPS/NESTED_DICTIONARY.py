ec6c45719dc8217e3b12558d81feb98eca037bc1
vagalemj <mj.vagale@gmail.com>
2023-07-11 02:59:06 +0530

    PYTHON

---
+++ HASHTABLE_OR_HASHMAPS/NESTED_DICTIONARY.py
@@ -0,0 +1,8 @@
+mj_dict = {'employee':
+               {'mj':
+                    {'ID': '001', 'salary': '20,000', 'designation': 'data engineer'},
+                'vagale':
+                    {'ID': '002', 'salary': '25,000', 'designation': 'data analyst'}
+                }
+           }            #nested dictionary
+print(mj_dict)
