4d100af64ee87904e4232aa2d1b4c627f3b7c1e0
vagalemj <mj.vagale@gmail.com>
2023-07-17 18:08:43 +0530

    PYTHON

---
+++ HASHTABLE_OR_HASHMAPS/DELETING_DICTIONARY_ITEMS.py
@@ -0,0 +1,13 @@
+mj_dict={'madhu':'001', 'sudhan':'002', 'vagale':'003', 'j':'004'}
+
+print(mj_dict)
+
+print(mj_dict.pop('sudhan'))    #pops specific key pair
+
+print(mj_dict.popitem())        #pops specific item
+
+del mj_dict['madhu']            #deletes the record
+
+print(mj_dict)
+
+print(mj_dict.clear())          #clears tho whole dictionary
