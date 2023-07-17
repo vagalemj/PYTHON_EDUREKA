103cb559acadb26d76c1d8a017452bf94250d86c
vagalemj <mj.vagale@gmail.com>
2023-07-17 18:46:45 +0530

    PYTHON

---
+++ OPERATORS/ARITHMETIC_OPERATORS.py
@@ -0,0 +1,10 @@
+a = 56
+b = 66
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Esponentiation:", a ** b)
+print("Division:", a / b)
+print("Flow Division:", a // b)
+print("Modulas:", a % b)
